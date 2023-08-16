ee4377279405845cead13d50e3d40cf5acbb02f4
Rosie Hayward <123747951+rh-camecon@users.noreply.github.com>
2023-08-16 19:25:03 +0100

    Update ftt_nmm_main.py
    See prev. commit.

--- SourceCode/Industrial_Heat/ftt_nmm_main.py
+++ SourceCode/Industrial_Heat/ftt_nmm_main.py
@@ -271,7 +271,7 @@ def solve(data, time_lag, iter_lag, titles, histend, year, domain):#, #specs, co
         division = np.zeros([len(titles['RTI']), len(titles['ITTI'])])
         division = divide((data_dt['IWK4'][:, :, 0] - data['IRG4'][:, :, 0]),
                           data_dt['IRG4'][:, :, 0])
-        isReg = 0.5 + 0.5*np.tanh(2*1.25*division)
+        isReg = 0.5 + 0.5*np.tanh(1.5+10*division)
         isReg[data['IRG4'][:, :, 0] == 0.0] = 1.0
         isReg[data['IRG4'][:, :, 0] == -1.0] = 0.0
 
