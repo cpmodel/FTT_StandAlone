8bb77a083103673785c3ae9435494a7007fc16a3
Femke Nijsse <femkemilene@posteo.net>
2023-11-03 13:17:27 +0000

    change structure
    Add model coupling folder to be populated later. Move variable_listing there

---
+++ SourceCode/Sector coupling/electricity_price.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  3 12:52:54 2023
+
+@author: Femke Nijsse
+"""
+
+def relative_electricity_price_movements(data, titles, histend, year, ftt_modules):
+    """ "This function takes as input the electricity price from FTT-P, and 
+    return the relative price of electricity at year x"""
+    
+    pass
+
+def electricity_demand_price_elasticity(data, titles, histend, year, ftt_modules):
+    """
+    Compute electricity demand changes using the
+    econometrically estimated elasticity. This is found in the X databank 
+    under the BFRE estimated parameters. 
+    
+    The equation in E3ME is in COINT. I think it 
+    """
+    pass
+
--- SourceCode/Sector coupling/energy_demand.py
+++ SourceCode/Sector coupling/energy_demand.py
@@ -51,13 +51,5 @@ def energy_demand_from_sectors(data, titles, histend, year, ftt_modules):
         
     return data
 
-def electricity_demand_price_elasticity(data, titles, histend, year, ftt_modules):
-    """
-    Compute electricity demand changes using the
-    econometrically estimated elasticity. This is found in the X databank 
-    under the BFRE estimated parameters. 
-    
-    The equation in E3ME is in COINT. I think it 
-    """
-    pass
+
 
