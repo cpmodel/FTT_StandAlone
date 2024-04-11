9e8cd4cfb2fd8f2d1ed294a21e3ea1d31ed3e7d9
olelouch <101116931+olelouch@users.noreply.github.com>
2024-04-11 08:48:16 -0400

    Debug + Another Try

--- SourceCode/Power/ftt_p_cmdem.py
+++ SourceCode/Power/ftt_p_cmdem.py
@@ -4,14 +4,19 @@ Created on Fri Mar 29 14:25:47 2024
 
 @author: wb591318
 """
-
+import copy
 import numpy as np
 
 
 def cmdem(data, titles):
     
+    cmdem_copy = copy.deepcopy(data['CMIN'])
+    # for i in range(len(data['CMIN'][:, :, :])):
+    #     cmdem_copy[:, :, i] =  cmdem_copy[:, :, i] * data['MEWI'][:, :, 0]
 
-    data['CMDEM'] = data['CMIN'] * np.broadcast_to(data['MEWI'], data['CMIN'].shape)
+    cmdem_copy = cmdem_copy * data['MEWI']
+
+    data['CMDEM'] = cmdem_copy
     
     return data
 
