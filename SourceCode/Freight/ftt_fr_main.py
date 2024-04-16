c0a9b0fc1f72bf41eff567cb14183177f158a0b6
cormacmlynch <122904574+cormacmlynch@users.noreply.github.com>
2024-04-16 13:59:11 +0100

    Removing ZLER
    Learning exponents were stored in two different places in FTT-Fr which could cause confusion. Now, ZLER is removed, and learning rates are stored just in the cost matrix, consistent with FTT-Tr.
    
    + some small clarifications to policy variables in the spreadsheets

--- SourceCode/Freight/ftt_fr_main.py
+++ SourceCode/Freight/ftt_fr_main.py
@@ -384,8 +384,9 @@ def solve(data, time_lag, iter_lag, titles, histend, year, domain):
                 if data['ZEWW'][0, tech, 0] > 0.1:
 
                     data['ZCET'][:, tech, c6ti['1 Price of vehicles (USD/vehicle)']] =  \
-                            data_dt['ZCET'][:, tech, c6ti['1 Price of vehicles (USD/vehicle)']] * \
-                            (1.0 + data['ZLER'][tech] * dw[tech]/data['ZEWW'][0, tech, 0])
+                            data_dt['ZCET'][:, tech, c6ti['1 Price of vehicles (USD/vehicle)']] \
+                            * (1.0 + data["ZCET"][:, tech, c6ti['15 Learning exponent']]
+                            * dw[tech]/data['ZEWW'][0, tech, 0])
 
 
             # Calculate total investment by technology in terms of truck purchases
