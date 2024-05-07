Fix MTFT calculation
It was using fuel costs, rather than fuel tax/subsidy

SourceCode/Power/ftt_p_lcoe.py
@@ -140,8 +140,8 @@ def get_lcoe(data, titles):
 
         # fuel tax/subsidies
         fft = np.ones([len(titles['T2TI']), int(max_lt)])
-        fft = ft * data['MTFT'][r, :, 0, np.newaxis]
-        fft = np.where(lt_mask, ft, 0)
+        fft = fft * data['MTFT'][r, :, 0, np.newaxis]
+        fft = np.where(lt_mask, fft, 0)
 
         # Average operation & maintenance cost
         omt = np.ones([len(titles['T2TI']), int(max_lt)])