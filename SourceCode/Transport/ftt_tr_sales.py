00285779bca57e1171ff2a5af874ca5c6c0611ff
Femke Nijsse <femkemilene@posteo.net>
2024-05-07 17:29:59 +0100

    start vectorising / improving tewi
    Improve code for TEWI in preparation of creating single bit of shared code

--- SourceCode/Transport/ftt_tr_sales.py
+++ SourceCode/Transport/ftt_tr_sales.py
@@ -30,51 +30,72 @@ def get_sales(data, data_dt, time_lag, titles, dt, c3ti, t):
     Parameters
     -----------
     data: dictionary
-        Data is a container that holds all cross-sectional (of time) for all
-        variables. Variable names are keys and the values are 3D NumPy arrays.
+        data holds the current state of all variables.
+        Variable names are keys and the values are 3D NumPy arrays.
     data_dt: dictionary
-        Data_dt is a container that holds all cross-sectional (of time) data
-        for all variables of the previous iteration.
+        data_dt is a container that for the previous timestep of all variables.
     time_lag: dictionary
-        Time_lag is a container that holds all cross-sectional (of time) data
+        time_lag is a container that holds all cross-sectional (of time) data
         for all variables of the previous year.
     titles: dictionary
         Titles is a container of all permissible dimension titles of the model.
-    dt: integer
-        Dt is an integer - 1 / number of iterations.
+    dt: float
+        dt is an float - 1 / number of iterations.
 
     Returns
     ----------
     data: dictionary
-        Data is a container that holds all cross-sectional (of time) data for
-        all variables.
-        Variable names are keys and the values are 3D NumPy arrays.
-        The values inside the container are updated and returned to the main
-        routine.
+        data now includes an updated TEWI or sales variable.
     tewi_t: 
+        tewi_t is a 3D NumPy array that holds the new sales/additions at time t.
     
     """
     # Find the difference in capacity between this iteration and last
     cap_diff = data['TEWK'][:, :, 0] - data_dt['TEWK'][:, :, 0]
     # Initialise variable for amount of capacity depreciated
-    cap_drpctn = np.zeros([len(titles['RTI']), len(titles['VTTI']), 1])
+    #cap_drpctn = np.zeros([len(titles['RTI']), len(titles['VTTI']), 1])
     # If capacity has grown, additions equal the difference +
     # depreciation from the previous year.
     # Otherwise, additions just equal depreciations.
     # Where capacity has decreased, we only add new capacity
     # if the depreciation > capacity loss
+
+    # Calculate share growth
+    share_growth = data["TEWS"][:, :, 0] - data_dt["TEWS"][:, :, 0]
+
+    # Calculate depreciation
+    depreciation = -data_dt["TEWS"][:, :, 0] * dt / data['BTTC'][:, :, c3ti['8 lifetime']]
+
+    # Calculate new capacity where capacity has grown
+    new_capacity = np.where(
+        cap_diff > 0.0,
+        data_dt["TEWK"][:, :, 0] * dt / data['BTTC'][:, :, c3ti['8 lifetime']],
+        0)
+
+    # Calculate new capacity where depreciation > share growth
+    new_capacity_growth = np.where(
+        (depreciation < share_growth) & (share_growth < 0), 
+        (share_growth + depreciation) * time_lag["TEWK"][:, :, 0],
+         0)
+
+    # Combine the two to get the total new capacity
+    cap_drpctn = new_capacity + new_capacity_growth
+
+
     for r in range(len(titles['RTI'])):
         for veh in range(len(titles['VTTI'])):
-            cap_drpctn[r,veh,0] = np.where(cap_diff[r, veh] > 0.0,
-                                data_dt["TEWK"][r, veh, 0] * dt / data['BTTC'][r, veh, c3ti['8 lifetime']],
-                                np.where((-data_dt["TEWS"][r, veh, 0] * dt / data['BTTC'][r, veh, c3ti['8 lifetime']] 
-                                          < data["TEWS"][r, veh, 0] - data_dt["TEWS"][r, veh, 0] < 0),
-                                            (data["TEWS"][r, veh, 0] - data_dt["TEWS"][r, veh, 0] 
-                                             + data_dt["TEWS"][r,veh,0] * dt / data['BTTC'][r, veh, c3ti['8 lifetime']])
-                                            * time_lag["TEWK"][r, veh, 0], 0))
-            # Catch any negative values
-            if cap_drpctn[r, veh, 0] < 0:
-                cap_drpctn[r, veh, 0] = 0
+            cap_drpctn[r, veh, 0] = np.where(
+                cap_diff[r, veh] > 0.0,
+                data_dt["TEWK"][r, veh, 0] * dt / data['BTTC'][r, veh, c3ti['8 lifetime']],
+                np.where((-data_dt["TEWS"][r, veh, 0] * dt / data['BTTC'][r, veh, c3ti['8 lifetime']] 
+                          < data["TEWS"][r, veh, 0] - data_dt["TEWS"][r, veh, 0] < 0),
+                            (data["TEWS"][r, veh, 0] - data_dt["TEWS"][r, veh, 0] 
+                             + data_dt["TEWS"][r, veh, 0] * dt / data['BTTC'][r, veh, c3ti['8 lifetime']])
+                            * time_lag["TEWK"][r, veh, 0], 0)
+                )
+            
+    # Ensure no negative values
+    cap_drpctn = np.maximum(cap_drpctn, 0)
 
     # Find total additions at time t
     tewi_t = np.zeros([len(titles['RTI']), len(titles['VTTI']), 1],)
@@ -84,8 +105,8 @@ def get_sales(data, data_dt, time_lag, titles, dt, c3ti, t):
         
     # Reset new additions if first FTT iteration
     if (t == 1):
-        data["TEWI"][: ,: ,0] = 0
+        data["TEWI"][:, :, 0] = 0
     # Add additions at iteration t to total annual additions
-    data["TEWI"][:, :, 0] = data["TEWI"][:,:,0] + tewi_t[:,:,0]
+    data["TEWI"][:, :, 0] = data["TEWI"][:, :, 0] + tewi_t[:, :, 0]
 
     return data, tewi_t
