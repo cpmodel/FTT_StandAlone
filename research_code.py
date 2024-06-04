bf97d62ec458ff71955b2268eb714e68675ab604
Rosie Hayward <123747951+rhayward1@users.noreply.github.com>
2024-06-04 16:05:46 +0200

    Updated automation code

--- research_code.py
+++ research_code.py
@@ -1,31 +1,46 @@
 '''Code snippets which will be useful for gamma automation'''
+# %%
 # Third party imports
 import numpy as np
 from tqdm import tqdm
-
-# Local library imports
-# Separate FTT modules
 
 from SourceCode import model_class
 
 scenario = 'S0'
 # %%
 model = model_class.ModelRun()
+modules = model.ftt_modules
 # %%
-model.timeline = np.arange(model.histend['MEWG']-5, model.histend['MEWG']+5)
-years = list(model.timeline)
-years = [int(x) for x in years]
-model.output = {scenario: {var: np.full_like(model.input[scenario][var][:,:,:,:10], 0) for var in model.input[scenario]}}
-for year_index, year in enumerate(model.timeline):
-                model.variables, model.lags = model.solve_year(year,year_index,scenario)
+# Identifying the variables needed for automation
+automation_var_list = model.titles['Models_shares_var'] + model.titles['Models_shares_roc_var'] + model.titles['Models_gamma_var']
+share_variables = dict(zip(model.titles['Models'] , model.titles['Models_shares_var']))
+roc_variables = dict(zip(model.titles['Models'] ,model.titles['Models_shares_roc_var']))
+gamma_variables = dict(zip(model.titles['Models'] ,model.titles['Models_gamma_var']))
+histend_vars = dict(zip(model.titles['Models'] , model.titles['Models_histend_var']))
+# %%
+# Looping through all FTT modules
+for module in modules:
 
-                # Populate output container
-                for var in model.variables:
-                    if 'TIME' in model.dims[var]:
-                        model.output[scenario][var][:, :, :, year_index] = model.variables[var]
-                    else:
-                        model.output[scenario][var][:, :, :, 0] = model.variables[var]
+    # Establisting timeline for automation algorithm
+    model.timeline = np.arange(model.histend[histend_vars[module]]-5, model.histend[histend_vars[module]]+5)
+    years = list(model.timeline)
+    years = [int(x) for x in years]
 
-shares = model.output['S0']['MEWS']
+    # Initialising container for automation variables
+    automation_varibales = {var: np.full_like(model.input[scenario][var][:,:,:,:len(model.timeline)], 0) for var in automation_var_list}
 
-shares
+    # Looping through years in automation timeline
+    for year_index, year in enumerate(model.timeline):
+
+        # Solving the model for each year
+        model.variables, model.lags = model.solve_year(year,year_index,scenario)
+
+        # Populate variable container for automation
+        for var in automation_var_list:
+            if 'TIME' in model.dims[var]:
+                automation_varibales[var][:, :, :, year_index] = model.variables[var]
+            else:
+                automation_varibales[var][:, :, :, 0] = model.variables[var]
+
+   
+
