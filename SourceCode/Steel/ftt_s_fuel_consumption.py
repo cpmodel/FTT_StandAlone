5c66bf030b4a22754c1b0b4520a63c08767e09d8
arpangolechha <162699719+arpangolechha@users.noreply.github.com>
2024-06-11 16:23:05 +0100

    Fuel consumption file errors solved
    Femke solvedthe error with sjef in the fuel consumption code and now the baseline runs.

--- SourceCode/Steel/ftt_s_fuel_consumption.py
+++ SourceCode/Steel/ftt_s_fuel_consumption.py
@@ -21,26 +21,26 @@ The outputs of this module include changes in final fuel consumption for the ful
 import numpy as np
 
 def ftt_s_fuel_consumption(bstc, sewg, smed, nrti, nstti, c5ti, njti):
-    sjef = np.zeros((nrti , njti, 0))
+    sjef = np.zeros((nrti , njti, 1))
     sjco = np.zeros_like((sjef))
     for r in range(nrti):
         for i in range(nstti):
         # Calculate fuel consumption
                 
-            sjef[r,0,0] += bstc[r, i,c5ti["Hard Coal"]] * sewg[r, i, 0]* 1000 * smed[0,11,0] * 1/41868
-            sjef[r,1,0] += bstc[r,i,c5ti["Other Coal"]] * sewg[r, i, 0]* 1000 * smed[0,12,0] * 1/41868
-            sjef[r,6,0] += bstc[r,i,c5ti["Natural Gas"]] * sewg[r, i, 0]* 1000 * smed[0,13,0] * 1/41868
-            sjef[r,7,0] += bstc[r,i,c5ti["Electricity"]] * sewg[r, i, 0]* 1000 * smed[0,14,0] * 1/41868
-            sjef[r,10,0] += ((bstc[r,i,c5ti["Biocharcoal"]] * sewg[r, i, 0]* 1000 * smed[0,18,0] * 1/41868) + (bstc[r,i,c5ti["Biogas"]] * sewg[r, i, 0]* 1000 * smed[0,19,0] * 1/41868))
-            sjef[r,11,0] += bstc[r,i,c5ti["Hydrogen"]] * sewg[r, i, 0]* 1000 * smed[0,17,0] * 1/41868
+            sjef[r, 0, 0] += bstc[r, i,c5ti["Hard Coal"]] * sewg[r, i, 0]* 1000 * smed[0,11,0] * 1/41868
+            sjef[r, 1, 0] += bstc[r,i,c5ti["Other Coal"]] * sewg[r, i, 0]* 1000 * smed[0,12,0] * 1/41868
+            sjef[r, 6, 0] += bstc[r,i,c5ti["Natural Gas"]] * sewg[r, i, 0]* 1000 * smed[0,13,0] * 1/41868
+            sjef[r, 7, 0] += bstc[r,i,c5ti["Electricity"]] * sewg[r, i, 0]* 1000 * smed[0,14,0] * 1/41868
+            sjef[r, 10, 0] += ((bstc[r,i,c5ti["Biocharcoal"]] * sewg[r, i, 0]* 1000 * smed[0,18,0] * 1/41868) + (bstc[r,i,c5ti["Biogas"]] * sewg[r, i, 0]* 1000 * smed[0,19,0] * 1/41868))
+            sjef[r, 11, 0] += bstc[r,i,c5ti["Hydrogen"]] * sewg[r, i, 0]* 1000 * smed[0,17,0] * 1/41868
             
-            if (bstc[r,i,21] == 1):
-                sjco[r,0,0] += 0.1 * bstc[r,i,c5ti["Hard Coal"]]*sewg[r,i,0]* 1000 * smed[0,11,0]*1/41868
-                sjco[r,1,0] += 0.1 * bstc[r,i,c5ti["Other Coal"]] * sewg[r, i, 0]* 1000 * smed[0,12,0] * 1/41868
-                sjco[r,6,0] += 0.1 * bstc[r,i,c5ti["Natural Gas"]] * sewg[r, i, 0]* 1000 * smed[0,13,0] * 1/41868
-                sjco[r,7,0] += bstc[r,i,c5ti["Electricity"]] * sewg[r, i, 0]* 1000 * smed[0,14,0] * 1/41868
-                sjco[r,10,0] += -0.9 * ((bstc[r,i,c5ti["Biocharcoal"]] * sewg[r, i, 0]* 1000 * smed[0,14,0] * 1/41868) + (bstc[r,i,c5ti["Biogas"]] * sewg[r, i, 0]* 1000 * smed[0,19,0] * 1/41868))
-                sjco[r,11,0] += bstc[r,i,c5ti["Hydrogen"]] * sewg[r, i, 0]* 1000 * smed[0,17,0] * 1/41868
+            if (bstc[r, i, 21] == 1):
+                sjco[r, 0, 0] += 0.1 * bstc[r,i,c5ti["Hard Coal"]]*sewg[r,i,0]* 1000 * smed[0,11,0]*1/41868
+                sjco[r, 1, 0] += 0.1 * bstc[r,i,c5ti["Other Coal"]] * sewg[r, i, 0]* 1000 * smed[0,12,0] * 1/41868
+                sjco[r, 6, 0] += 0.1 * bstc[r,i,c5ti["Natural Gas"]] * sewg[r, i, 0]* 1000 * smed[0,13,0] * 1/41868
+                sjco[r, 7, 0] += bstc[r,i,c5ti["Electricity"]] * sewg[r, i, 0]* 1000 * smed[0,14,0] * 1/41868
+                sjco[r, 10, 0] += -0.9 * ((bstc[r,i,c5ti["Biocharcoal"]] * sewg[r, i, 0]* 1000 * smed[0,14,0] * 1/41868) + (bstc[r,i,c5ti["Biogas"]] * sewg[r, i, 0]* 1000 * smed[0,19,0] * 1/41868))
+                sjco[r, 11, 0] += bstc[r,i,c5ti["Hydrogen"]] * sewg[r, i, 0]* 1000 * smed[0,17,0] * 1/41868
             
             else:
                 sjco[r,0,0] += bstc[r,i,c5ti["Hard Coal"]]*sewg[r,i,0]* 1000 * smed[0,11,0]*1/41868
