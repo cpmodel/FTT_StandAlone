945bbce52ba8d7e6aed5579ce1ba78dda53b130e
Femke Nijsse <femkemilene@posteo.net>
2024-06-19 11:22:48 +0100

    Fix errors in coding
    First results are now in: there is often no cross-over year

--- Analysis/Assess_crossover_price.py
+++ Analysis/Assess_crossover_price.py
@@ -30,7 +30,7 @@ os.chdir(project_root_absolute_path)
 # source_code_path = os.path.join(project_root_absolute_path, 'SourceCode')
 # sys.path.append(source_code_path)
 
-pickle_path = os.path.join(project_root_absolute_path, 'Output\\Results2.pickle') 
+pickle_path = os.path.join(project_root_absolute_path, 'Output\\Results.pickle') 
 
 with open(pickle_path, 'rb') as f:
     results = pickle.load(f)
@@ -73,15 +73,18 @@ titles = load_titles()
 
 # Define the clean technology list by model
 clean_techs = {"FTT:P": [16, 18], "FTT:Tr": [18, 19, 20], "FTT:H": [10, 11], "FTT:Fr": [12]}
-dirty_techs = {"FTT:P": [0, 2, 6], "FTT:Tr": list(range(12)), "FTT:H": [3, 4], "FTT:Fr": [0, 2, 4, 6, 8]}
+dirty_techs = {"FTT:P": [0, 2, 6], "FTT:Tr": list(range(12)), "FTT:H": [2, 3], "FTT:Fr": [0, 2, 4, 6, 8]}
 
 # Define the year of interest
 year = 2030
 
-# Define the price of interest
+# Define the shares, prices of interest
 models = {"FTT:P", "FTT:Tr", "FTT:H", "FTT:Fr"}
 price_names = {"FTT:P": "MECW", "FTT:Tr": "TEWC", "FTT:H": "HEWC", "FTT:Fr": "ZTLC"}
 shares_names = {"FTT:P": "MEWS", "FTT:Tr": "TEWS", "FTT:H": "HEWS", "FTT:Fr": "ZEWS"}
+
+# Get names of the technologies of interest
+tech_titles = {"FTT:P": "T2TI", "FTT:Tr": "VTTI", "FTT:Fr": "FTTI", "FTT:H": "HTTI"}
 
 # Construct a dataframe with the biggest clean and dirty technologies.
 # The dataframe will have the following columns:
@@ -94,7 +97,7 @@ shares_names = {"FTT:P": "MEWS", "FTT:Tr": "TEWS", "FTT:H": "HEWS", "FTT:Fr": "Z
 # - Cross-over year if any
 
 # Find the biggest clean or fossil technology:
-def find_biggest_tech(output, tech_lists, year, model):
+def find_biggest_tech(output, tech_lists, year, model, regions):
     """Find the biggest technology in each region for a given model."""
     shares_var = shares_names[model]
     tech_list = tech_lists[model]
@@ -108,18 +111,35 @@ def find_biggest_tech(output, tech_lists, year, model):
                 max_techs[r] = tech
     return max_techs
 
+
+# Find the biggest clean or fossil technology:
+def find_biggest_tech_dirty(output, dirty_techs, biggest_techs_clean, year, model):
+    """Find the biggest technology in each region for a given model."""
+    
+    if model != "FTT:Tr":
+        max_techs = find_biggest_tech(output, dirty_techs, year, model, regions)
+        return max_techs
+    
+    max_techs = {}
+    for r, ri in regions.items():
+        # For FTT:Tr, only compare vehicles in the same luxery category
+        biggest_tech_clean = (r, biggest_techs_clean[r])
+        dirty_techs = remove_vehicles_from_list(dirty_techs, biggest_tech_clean)
+        max_techs.update(find_biggest_tech(output, dirty_techs, year, model, {r: ri}))
+    return max_techs
+
 def remove_vehicles_from_list(dirty_techs, biggest_techs_clean):
     """ Remove cars in different luxery categories from consideration."""
     if model != "FTT:Tr":
         return dirty_techs
     
-    for r, tech in biggest_techs_clean.items():
-        if tech == 18:
-            dirty_techs["FTT:Tr"] = [0, 3, 6, 9]
-        if tech == 19:
-            dirty_techs["FTT:Tr"] = [1, 4, 7, 10]
-        if tech == 20:
-            dirty_techs["FTT:Tr"] = [2, 5, 8, 11]
+    r, tech = biggest_techs_clean
+    if tech == 18:
+        dirty_techs["FTT:Tr"] = [0, 3, 6, 9]
+    elif tech == 19:
+        dirty_techs["FTT:Tr"] = [1, 4, 7, 10]
+    elif tech == 20:
+        dirty_techs["FTT:Tr"] = [2, 5, 8, 11]
     return dirty_techs
         
         
@@ -139,39 +159,44 @@ def get_crossover_year(output, model, biggest_techs_clean, biggest_techs_dirty):
         tech_clean = biggest_techs_clean[r]
         tech_dirty = biggest_techs_dirty[r]
         try:
-            price_series_clean = output[price_names[model]][ri, tech_clean, 0, :]
-            price_series_dirty = output[price_names[model]][ri, tech_dirty, 0, :]
-            crossover_years[r] = np.where(price_series_clean <= price_series_dirty)[0][0] + 2010
-        except IndexError as e:
+            price_series_clean = output[price_names[model]][ri, tech_clean, 0, 10:]
+            price_series_dirty = output[price_names[model]][ri, tech_dirty, 0, 10:]
+            crossover_years[r] = np.where(price_series_clean <= price_series_dirty)[0][0] + 2020
+        except IndexError:
             crossover_years[r] = None
     return crossover_years
 
 
 corss_overs = get_crossover_year(
     output, "FTT:P", 
-    find_biggest_tech(output, clean_techs, year, "FTT:P"), find_biggest_tech(output, dirty_techs, year, "FTT:P"))
+    find_biggest_tech(output, clean_techs, year, "FTT:P", regions), find_biggest_tech(output, dirty_techs, year, "FTT:P", regions))
 
-# Construct the dataframe
-df = pd.DataFrame(columns=["Region", "Sector", "Clean technology", "Dirty technology", "Clean price (2020)", "Dirty price (2020)", "Cross-over year"])
+
 rows = []
 for model in models:
-    biggest_techs_clean = find_biggest_tech(output, clean_techs, year, model)
-    dirty_techs = remove_vehicles_from_list(dirty_techs, biggest_techs_clean)
-    biggest_techs_dirty = find_biggest_tech(output, biggest_techs_clean, dirty_techs, year, model)
+    biggest_techs_clean = find_biggest_tech(output, clean_techs, year, model, regions)
+    biggest_techs_fossil = find_biggest_tech_dirty(output, dirty_techs, biggest_techs_clean, year, model)
+    clean_tech_names = {key: titles[tech_titles[model]][index] for key, index in biggest_techs_clean.items()}
+    fossil_tech_names = {key: titles[tech_titles[model]][index] for key, index in biggest_techs_fossil.items()}
     prices_clean = get_prices(output, year, model, biggest_techs_clean)
-    prices_dirty = get_prices(output, year, model, biggest_techs_dirty)
-    crossover_years = get_crossover_year(output, model, biggest_techs_clean, biggest_techs_dirty)
+    prices_dirty = get_prices(output, year, model, biggest_techs_fossil)
+    crossover_years = get_crossover_year(output, model, biggest_techs_clean, biggest_techs_fossil)
     for r in regions:
         row = {
         "Region": r, 
         "Sector": model, 
         "Clean technology": biggest_techs_clean[r], 
-        "Dirty technology": biggest_techs_dirty[r], 
-        "Clean price (2020)": prices_clean[r], 
-        "Dirty price (2020)": prices_dirty[r],
+        "Clean tech name": clean_tech_names[r],
+        "Clean price (2030)": prices_clean[r], 
+        "Fossil technology": biggest_techs_fossil[r], 
+        "Fossil tech name": fossil_tech_names[r],
+        "Fossil price (2030)": prices_dirty[r],
         "Cross-over year": crossover_years[r]
         }
         rows.append(row)
 
 # Construct the DataFrame from the list of dictionaries
-df = pd.DataFrame(rows, columns=["Region", "Sector", "Clean technology", "Dirty technology", "Clean price (2020)", "Dirty price (2020)", "Cross-over year"])
+df = pd.DataFrame(rows, columns=["Region", "Sector",
+                                 "Clean technology", "Clean tech name", "Clean price (2030)", 
+                                 "Fossil technology", "Fossil tech name", "Fossil price (2030)", 
+                                 "Cross-over year"])
