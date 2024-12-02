54a09ed6311c5c655eeb7bef7a5791492958738f
Pim Vercoulen <pv@camecon.com>
2024-12-02 22:55:21 +0100

    Adding empty scripts
    Architectural stuff

---
+++ SourceCode/core_functions/generic_cost_supply_curves.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 15:47:20 2024
+
+@author: pv
+"""
---
+++ SourceCode/core_functions/pooled_trading.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 22:17:38 2024
+
+@author: pv
+"""
