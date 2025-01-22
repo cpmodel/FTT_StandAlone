f043f1b452b622e7bbc165a42e6902e9ee2d154e
Femke Nijsse <femkemilene@posteo.net>
2025-01-22 09:36:35 +0000

    Prettier graph
    Also some tidying up of unused code.

--- Analysis/cost_declines_by_policy.py
+++ Analysis/cost_declines_by_policy.py
@@ -84,93 +84,18 @@ def get_weighted_percentage_difference(output, model,
 output_S0 = get_output(output_file, "S0")
 
 
-#%% =============================================================================
-# Cost declines wrt to 2024 at 2035 and 2050 (omitted in favour of different graph)
-# ==================================================================================
-
-plot_dot_graph = False
-
-if plot_dot_graph:
-    df_dict = {}         # Creates a dict later filled with dataframes
-
-    for model in models:
-        df_dict[model] = pd.DataFrame()
-        rows = []
-        
-        # Get the bit of the model name after the colon (like Fr)
-        model_abb = model.split(':')[1]
-        output_ct = get_output(output_file, f"sxp - {model_abb} CT")
-        output_sub = get_output(output_file, f"sxp - {model_abb} subs")
-        output_man = get_output(output_file, f"sxp - {model_abb} mand")
-        
-        scenarios = {"Current traj.": output_S0, "Carbon tax": output_ct,
-                     "Subsidies": output_sub, "Mandates": output_man}
-        
-        for scen, output in scenarios.items():
-            weighted_prices = get_weighted_costs(output, model, tech_variable[model], year_inds)
-            normalised_prices = weighted_prices / weighted_prices[0]
-            
-            row = {"Scenario": scen, "Price 2035": normalised_prices[1], "Price 2050": normalised_prices[2]}
-            rows.append(row)
-        
-        df_dict[model] = pd.DataFrame(rows)
-        
-    
-    # Cost declines 2035/2050 -- plotting
-    
-    fig, axs = plt.subplots(2, 2, figsize=(3.5, 5), sharey=True)
-    axs = axs.flatten()
-    palette = sns.color_palette("Blues_r", 3)
-    
-    
-    for mi, model in enumerate(models):
-        df = df_dict[model]
-        ax = axs[mi]
-        ax.plot(df["Scenario"], df["Price 2035"], 'o', label='Price 2035', markersize=15, color=palette[0])
-        ax.plot(df["Scenario"], df["Price 2050"], 'o', label='Price 2050', markersize=15, color=palette[1])
-    
-        # Add labels and title
-        ax.set_xticklabels(df["Scenario"], rotation=90)
-        if mi % 2 == 0:
-            ax.set_ylabel('Cost relative to 2024')
-        ax.set_title(tech_name[model], pad=20)
-        
-        # Remove frame
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
-        ax.spines['bottom'].set_visible(False)
-        ax.spines['left'].set_visible(False)
-        
-        ax.xaxis.set_ticks_position('none') 
-        ax.yaxis.set_ticks_position('none') 
-        
-        ax.set_ylim(0.45, 1.)
-        ax.grid(axis='y')
-        
-    
-    # Add legend only to the last plot
-    handles, labels = ax.get_legend_handles_labels()
-    fig.subplots_adjust(hspace=1, wspace=200)
-    fig.legend(handles, labels, loc='upper right', frameon=False, ncol=2)
-    
-    plt.tight_layout(rect=[0, 0, 1, 0.95])  # Adjust layout to make space for the legend
-    plt.show()
-       
-    # Save the graph as an editable svg file
-    save_fig(fig, fig_dir, "Cost_declines_by_policy")
-
-
 
 #%% =====================================================================
 # Globally averaged cost difference over time by within-sector policy
 # ========================================================================
 
 clean_tech_variable = {"FTT:P": 18, "FTT:Tr": 19, "FTT:H": 10, "FTT:Fr": 33}
-fossil_tech_variable = {"FTT:P": 2, "FTT:Tr": 1, "FTT:H": 3, "FTT:Fr": 13} # Note 4 for transport gives an error
-graph_label = {"FTT:P": "New solar + battery \n vs existing coal", "FTT:H": "Water-air HP \n vs gas boiler",
-               "FTT:Tr": "Electric vehicles \n vs petrol cars", "FTT:Fr": "Electric trucks \n vs diesel trucks"}
+fossil_tech_variable = {"FTT:P": 2, "FTT:Tr": 1, "FTT:H": 3, "FTT:Fr": 13} 
+graph_label = {"FTT:P": "New solar + battery vs existing coal", "FTT:H": "Air-to-water HP vs gas boiler",
+               "FTT:Tr": "Electric vehicles vs petrol cars", "FTT:Fr": "Electric trucks vs diesel trucks"}
 
-   
+price_diff_perc_by_region = {}
+weights_by_region = {} 
 year_inds = list(range(13, 41))
 timeseries_dict = {}
 
@@ -188,31 +113,27 @@ for model in models:
      
     
     for scen, output in scenarios.items():
-        # weighted_prices_clean = get_weighted_costs(output, model, clean_tech_variable[model], year_inds)
-        # weighted_prices_fossil = get_weighted_costs(output, model, fossil_tech_variable[model], year_inds)
-        # price_diff_perc = get_percentage_difference(weighted_prices_clean, weighted_prices_fossil)
-        # timeseries_by_policy.append(price_diff_perc)
-        
+               
         prices_clean = get_costs(output, model, clean_tech_variable[model], year_inds)
         prices_fossil = get_costs(output, model, fossil_tech_variable[model], year_inds)
+        price_diff_perc_by_region[model] = get_percentage_difference(prices_clean, prices_fossil)
+        weights_by_region[model] = np.sum(output[shares_vars[model]][:, :, 0, year_inds], axis=1)   
         price_diff_perc = get_weighted_percentage_difference(output, model, 
                                                prices_clean, prices_fossil, year_inds)
         timeseries_by_policy.append(price_diff_perc)
-        if model == "FTT:Fr":
-            test = 1
         
     
     timeseries_dict[model] = timeseries_by_policy
 
 #%% Global cost difference -- plotting
-fig, axs = plt.subplots(2, 2, figsize=(3.5, 3.5), sharey=True)
+fig, axs = plt.subplots(2, 2, figsize=(3.5, 3.8), sharey=True)
 axs = axs.flatten()
 
 # Get 4 colours from the "rocket" palette
-colours = sns.color_palette()
+colours = sns.color_palette("mako", 4)
 
 def custom_xaxis_formatter(x, pos):
-    if x in [2030, 2040]:
+    if x in [2030, 2040, 2050]:
         return f'{int(x)}'
     else:
         return ''
@@ -221,8 +142,19 @@ for mi, model in enumerate(models):
     ax = axs[mi]
 
     ax.axhline(y=0, color="grey", linewidth=2)    
+    
+    
     for si, (scen, colour) in enumerate(zip(scenarios.keys(), colours)):
-        ax.plot(range(2023, 2051), timeseries_dict[model][si], label=scen, color=colour, linewidth=1)
+        x_vals = range(2023, 2051)
+        y_vals = timeseries_dict[model][si]
+        ax.plot(x_vals, y_vals, label=scen, color=colour, linewidth=1)
+        
+        # Find zero crossings
+        for i in range(len(y_vals) - 1):
+            if y_vals[i] * y_vals[i + 1] < 0:  # Sign change indicates a zero crossing
+                # Linear interpolation for exact crossing point
+                x_cross = x_vals[i] + (0 - y_vals[i]) * (x_vals[i + 1] - x_vals[i]) / (y_vals[i + 1] - y_vals[i])
+                ax.axvline(x_cross, color=colour, linestyle="--", linewidth=1)
     
     
     # Remove frame
@@ -236,12 +168,12 @@ for mi, model in enumerate(models):
     
     if mi in [0, 2, 3]:
         ax.set_xlim(2024, 2030)
-        ax.grid(True, which='major', linewidth=0.7)
+        ax.grid(True, which='major', linewidth=0.3)
         ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
     else:
         ax.set_xlim(2024, 2050)
-        ax.grid(True, which='major', linewidth=0.7)
-        ax.grid(True, which='minor', linewidth=0.2)
+        ax.grid(True, which='major', linewidth=0.3)
+        ax.grid(True, which='minor', linewidth=0.05)
         ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
         ax.xaxis.set_minor_locator(ticker.MultipleLocator(2))
 
@@ -252,18 +184,17 @@ for mi, model in enumerate(models):
         # Apply the custom formatter to the x-axis
         ax.xaxis.set_major_formatter(ticker.FuncFormatter(custom_xaxis_formatter))
     
-    ax.grid(True, axis='y', linewidth=0.5)
+    ax.grid(True, axis='y', linewidth=0.3)
     
     if mi == 3:
-        ax.legend(loc='right')
+        ax.legend(loc='upper right')
     
     if mi in [0, 2]:
         ax.set_ylabel("Levelised cost difference (%)")
     
-    #ax.text(2033, 26, graph_label[model], ha="right")
-    ax.set_title(graph_label[model], pad=-15, ha="right", fontweight='bold')
-    title = ax.title
-    title.set_position((0.97, title.get_position()[1]))
+    ax.set_title(graph_label[model], ha="right")
+    ax.title.set_position((1.07, ax.title.get_position()[1]))
+    fig.subplots_adjust(hspace=.4)
 
     
 
