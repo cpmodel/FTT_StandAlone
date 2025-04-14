77a8026c5d027546e540593a99b867c46e78d97c
Femke Nijsse <femkemilene@posteo.net>
2025-04-14 14:55:58 +0100

    Reduce st in transport
    Don't think the type of car has an effect on the standard deviations.. Unlike power, where investors do take this into account

--- SourceCode/Transport/ftt_tr_lcot.py
+++ SourceCode/Transport/ftt_tr_lcot.py
@@ -211,7 +211,7 @@ def get_lcot(data, titles, carbon_costs, year):
         # MODIFIED: Calculate variance terms and apply proper discounting
         variance_terms = dit**2 + dft**2 + domt**2
         summed_variance = np.sum(variance_terms/(denominator**2), axis=1)
-        variance_plus_dcf = summed_variance + (np.sum(npv_expenses2, axis=1) * 0.15)**2
+        variance_plus_dcf = summed_variance + (np.sum(npv_expenses2, axis=1) * 0.1)**2
         dlcot = np.sqrt(variance_plus_dcf)/np.sum(npv_utility, axis=1)
 
         # 1-levelised cost variants in $/pkm
