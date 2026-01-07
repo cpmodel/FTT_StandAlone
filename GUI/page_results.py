bbacb078fe790d3bbb14305614c28ced3e936a92
Cormac Lynch <122904574+cormacmlynch@users.noreply.github.com>
2026-01-07 17:01:26 +0000

    More results page structure adjustments
    Reducing figure size and increasing size of interaction panel

--- GUI/page_results.py
+++ GUI/page_results.py
@@ -1,5 +1,6 @@
 from nicegui import ui
 from plotly import graph_objects as go
+import plotly.io as pio
 
 from .shared import shared_layout
 from .state import state
@@ -12,19 +13,23 @@ def render_results_page():
             # SECTION 1. Figure
             with ui.card().classes('w-full p-4 shadow-sm border border-gray-200'):
                 # determine whether dark mode enabled for plotting template
-                plt_tem = 'plotly_dark' if state.dark_mode else 'plotly'
-                fig = go.Figure(go.Scatter(x=[1, 2, 3, 4], y=[1, 2, 3, 2.5]))
-                fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), template = plt_tem)
-                plot = ui.plotly(fig).classes('w-full h-[300px]')
+                plt_tem = 'plotly_dark' if state.dark_mode else 'simple_white'
+                fig = go.Figure(go.Scatter(name='1 Belgium', x=[2020, 2030, 2040, 2050], y=[1, 2, 3, 2.5]))
+                fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), template = plt_tem, showlegend=True)
+                plot = ui.plotly(fig).classes('w-full h-[250px]')
+
+                print(list(pio.templates))
 
             # SECTION 2. Data selector
-            with ui.card().classes('w-full h-[190px] p-4 shadow-sm border border-gray-200'):
+            with ui.card().classes('w-full h-[240px] p-4 shadow-sm border border-gray-200'):
                 with ui.row().classes('w-full h-full'):
                     # set tabs
-                    with ui.tabs().props('vertical stretch=true indicator-color=blue-400').classes('h-full justify-center p-2 border-r') as tabs:
-                        t1 = ui.tab('1. Load Files')
-                        t2 = ui.tab('2. Scenarios')
-                        t3 = ui.tab('3. Variables')
+                    with ui.tabs().props('vertical stretch=true indicator-color=blue-400 dense=true').classes('h-full justify-center p-1 border-r') as tabs:
+                        t1 = ui.tab('Load Files')
+                        t2 = ui.tab('Scenarios')
+                        t3 = ui.tab('Variables')
+                        t4 = ui.tab('Chart Settings')
+
 
                     with ui.tab_panels(tabs, value=t1).classes('flex-grow h-full'):
                 
@@ -47,3 +52,28 @@ def render_results_page():
                                 ui.select(options=["a","b"], label = "Dim 1").classes('w-50')
                                 ui.select(options=["a","b"], label = "Dim 2").classes('w-50')
                                 ui.select(options=["a","b"], label = "Dim 3").classes('w-50')
+
+                        with ui.tab_panel(t4):
+                            with ui.row():
+                                grouped_options = [
+                                    {
+                                        'label': 'Fruits', # This is the header for the group
+                                        'children': [
+                                            {'label': 'Apple', 'value': 'apple'},
+                                            {'label': 'Banana', 'value': 'banana'},
+                                        ]
+                                    },
+                                    {
+                                        'label': 'Vegetables',
+                                        'children': [
+                                            {'label': 'Carrot', 'value': 'carrot'},
+                                            {'label': 'Potato', 'value': 'potato'},
+                                        ]
+                                    }
+                                ]
+
+                                ui.select(
+                                    options=grouped_options, 
+                                    with_input=True, 
+                                    label='Search Categories'
+                                ).classes('w-64').props('emit-value map-options')
