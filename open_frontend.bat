cde4ec872c03f01a816449e3daa1132262409cdd
Cormac Lynch <122904574+cormacmlynch@users.noreply.github.com>
2026-02-24 15:20:10 +0000

    frontend bat file formatting

--- open_frontend.bat
+++ open_frontend.bat
@@ -1,1 +1,10 @@
+@echo off
+echo.
+echo ==========================================
+echo Future technology transformations frontend
+echo.
+echo         Now opening. Please wait.....
+echo ==========================================
+echo.
 python run_frontend.py
+echo.
